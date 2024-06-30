4f9f26b0f7395d6df064488df5c6112bfa402681
Emre Öztürk <76410095+emreozturken@users.noreply.github.com>
2024-06-30 21:23:46 +0300

    👾 Inital commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# todo-app-vue
+<center >
+
+<img src="https://i.ibb.co/vYd0MN1/Saly-26-1.png" height="60" width="60"/>
+
+<h2>Todo App</h2>
+
+<p>My first nuxt & vue project for learning nuxt & vue ecosystem.
+</p>
+
+<img src="https://i.ibb.co/zQVdfjk/Screenshot-2024-06-30-at-21-17-53.png"/>
+
+<h2>Installation & dev</h2>
+
+<code>npm i && npm run dev</code>
+
+</center>
---
+++ app.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+const todoArray: Ref<string[]> = ref([]);
+const userInput: Ref<string | null> = ref(null);
+const error: Ref<string | null> = ref(null);
+
+const createTask = () => {
+  if (!userInput.value) return (error.value = "Please enter a task!");
+  todoArray.value.push(userInput.value);
+};
+</script>
+
+<template>
+  <form
+    @submit.prevent="createTask"
+    class="flex justify-center items-center h-screen flex-col"
+  >
+    <img src="/todo.png" class="h-20 mb-4" alt="Todo Logo" />
+    <h1 class="text-3xl mb-6">TODO App</h1>
+
+    <p class="text-rose-500" v-if="error">
+      {{ error }}
+    </p>
+
+    <input
+      type="text"
+      class="border rounded-lg p-2"
+      v-model="userInput"
+      placeholder="Enter a task please! 😊"
+    />
+
+    <button
+      type="submit"
+      class="mt-4 bg-green-500 p-2 border rounded-lg w-32 text-white"
+    >
+      Add Task
+    </button>
+
+    <div class="mt-10">
+      <p
+        class="font-bold mb-2 border bg-white rounded-lg p-6 w-96"
+        v-for="tasks of todoArray"
+      >
+        {{ tasks }}
+      </p>
+    </div>
+  </form>
+</template>
